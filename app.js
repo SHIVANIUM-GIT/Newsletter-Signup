dd0ceacf9079c794a146c0b2508ccaed74537aae
Shivanium <Shivrathore223@gmail.com>
2023-10-10 11:23:21 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function (req, res) {
+  console.log("Post received");
+  const firstname = req.body.fname;
+  const lastname = req.body.lname;
+  const email = req.body.email;
+
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstname,
+          LNAME: lastname,
+        },
+      },
+    ],
+  };
+
+  const jsonData = JSON.stringify(data);
+
+  const url = "https://us7.api.mailchimp.com/3.0/lists/4035cb3d88";
+
+  const options = {
+    method: "POST",
+    auth: "aditya:303e06f2d2c0c6e10979f6049305f16f-us7",
+  };
+
+  const request = https.request(url, options, function (response) {
+    if (response.statusCode === 200) {
+      res.sendFile(__dirname + "/success.html");
+    } else {
+      res.sendFile(__dirname + "/failure.html");
+    }
+
+    response.on("data", function (data) {
+      console.log(JSON.parse(data));
+    });
+  });
+  request.write(jsonData);
+  request.end();
+});
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log("Port is listening at http://localhost:3000");
+});
